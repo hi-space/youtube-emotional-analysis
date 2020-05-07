484d3091a7a96d32bbf0d8a29a3d84b3a35aecbd
hispace <hispace.j@gmail.com>
2020-05-08 01:47:54 +0900

    text cleaning job

--- youtube-comments-crawling/using_youtube_api/main.py
+++ youtube-comments-crawling/using_youtube_api/main.py
@@ -1,5 +1,8 @@
 from crawlling import *
+from text_cleaning import *
 from utils import * 
+from config import *
+
 
 def start_crawlling(d, origin_key=None):
     for k, v in d.items():
@@ -9,11 +12,14 @@ def start_crawlling(d, origin_key=None):
             log(origin_key + " / " + k + " / " + v)
             crawlling_comments(origin_key, k, v)
 
-def start_text_cleaning(directory_path):
+def start_text_cleaning(directory_path=STORE_FILE_PATH):
     file_list = file_list_in_directory(directory_path)
-
+    for file in file_list:
+        log('cleaning - ' + file)
+        refine_file(directory_path, PREPROCESSING_PATH, file)
 
 if __name__ == "__main__":
     # start_crawlling(VIDEO_IDS)
-    l = file_list_in_directory('''D:\\02-yscec\\2020-1\\DataVisualization\\final-term\\source_data\\show''')
-    print(l)
+    
+    start_text_cleaning('D:\\02-yscec\\2020-1\\DataVisualization\\final-term\\source_data\\interview\\')
+    
--- youtube-comments-crawling/using_youtube_api/text_cleaning.py
+++ youtube-comments-crawling/using_youtube_api/text_cleaning.py
@@ -1,4 +1,6 @@
 import re
+import pandas as pd
+from utils import log
 
 
 HANGUL = re.compile('[^ ㄱ-ㅣ가-힣]+')
@@ -7,16 +9,38 @@ LETTERS = re.compile('[^-가-힣a-zA-Z0-9/ ]')
 
 
 def is_hangul(s):
-    result = HANGUL.sub('', s).strip()
+    result = HANGUL.sub('', str(s)).strip()
     if len(result) == 0:
         return False
     return True
 
 def is_english(s):
-    result = ENGLISH.sub('', s).strip()
+    result = ENGLISH.sub('', str(s)).strip()
     if len(result) == 0:
         return False
     return True
 
 def refine_sentence(s):
-    return LETTERS.sub('', s)
+    return LETTERS.sub('', str(s))
+
+def refine_file(source_path, output_path, file):
+    try:
+        df = pd.read_csv(source_path + file)
+    except Exception:
+        log("Exception during reading file - " + source_path + file)
+
+    # remove letters without hangul or english
+    for idx, row in df.iterrows():
+        s = refine_sentence(row.contents)
+
+        if len(s.strip()) != 0:
+            df.at[idx, 'contents'] = s
+        else:
+            df.drop(idx, inplace=True)
+    
+    # save refined file
+    df.to_csv(output_path + file,
+                sep=',',
+                columns=['author', 'contents', 'likeCount', 'date', 'hangul'],
+                encoding='utf-8-sig')
+
