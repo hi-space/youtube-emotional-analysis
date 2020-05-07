6c79e86563738df298421e230205ef16804b9cba
hispace <hispace.j@gmail.com>
2020-05-07 09:34:50 +0900

    source lists config

---
+++ youtube-comments-crawling/all_config.py
@@ -0,0 +1,104 @@
+KEY=""
+STORE_FILE_PATH= ""
+VIDEO_IDS = {
+    'oh_my_girl': {
+        'nonstop' : 'iDjQSdN_ig8',
+        'the_fifth_season' : 'udGwca1HBM4',
+        'bungee' : 'QTD_yleCK9Y',
+    },
+    'chungha': {
+        'stay_tonight': 'YPFIh0dfYfw',
+        'roller_coaster': '900X9fDFLc4',
+        'snapping' : 'deV_DmHKwjc'
+    },
+    'mamamoo': {
+        'spit_it_out': 'bD06B-5SsSE',
+        'egotistic': 'pHtxTSiPh5I',
+        'starry_night' : '0FB2EoKTK_Q'
+    },
+    'g_idle': {
+        'oh_my_god': 'om3n2ni8luE',
+        'latata': '9mQk7Evt6Vs',
+        'hann' : 'OKNXn2qCEws'
+    },
+    'itzy': {
+        'wannabe': 'fE2h3lGlOsk',
+        'icy': 'zndvqTc4P9I',
+        'dalla_dalla' : 'pNfTK39k55U'
+    },
+    'nct': {
+        'ridin': 'vofjeJvRT9c',
+        'boom': 'X-iJZ0gfKPo',
+        'we_go_up' : 'LV1Es22E0RI'
+    },
+    'zico': {
+        'anysong': 'UuV2BmJ1p_I',
+        'icy': 'ewjucLierFc',
+        'i_am_you_you_are_me' : 'obzb7nlpXZ0'
+    },
+    'bts': {
+        'on': 'mPVDGOVjRQ0',
+        'dna': 'MBdVXkSdhwU',
+        'boy_with_love' : 'XsX3ATc3FbA'
+    },
+    'winner': {
+        'remember': 'NTJ8esMHW2E',
+        'everyday': 'd1D1SJ-KqaQ',
+        'really_really' : '4tBnF46ybZk'
+    },
+    'kangdaniel': {
+        '2u': 'GmqfRBaJm6I',
+        'what_are_you_up_to': '_-QY40Reub8',
+        'touchin' : 'aSGisoCdLPc'
+    },
+    'in_real_life': {
+        'she_do': 'TXTMIhhdRpE',
+        'tonight_belongs_to_you': 'xARyV3h1SB0',
+        'tatoo' : 'TqDg-PLCJiE'
+    },
+    'prettymuch': {
+        'blind': 'j4FL7CMustE',
+        'teacher': 'PLyMj7k5Ths',
+        'me_necesita' : 'M7j3J1sTDFc'
+    },
+    'onedirection': {
+        'perfect': 'Ho32Oh6b4jc',
+        'drag_me_down': 'Jwgf3wmiA04',
+        'history' : 'yjmp8CoZBIo'
+    },
+    'justin_bieber': {
+        'yummy': '8EJ3zbKTWQ8',
+        'what_do_you_mean': 'DK_0jXPuIr0',
+        'sorry' : 'fRh_vgS2dFE'
+    },
+    'why_dont_we': {
+        'chills': 'wTdZbbx53uk',
+        '8letters': 'C3DlM19x4RQ',
+        'trust_fund_baby' : 'fOmhbMXY52o'
+    },
+    'little_mix': {
+        'wasabi': 'ee5aEU4XEnc',
+        'shout_out_to_my_ex': 'bFDzhKdrN9M',
+        'power' : 'Dw8B1q1tKgs'
+    },
+    'dualipa': {
+        'break_my_heart': 'Nj2U6rhnucI',
+        'dont_start_now': 'oygrmJFKYZY',
+        'new_rules' : 'k2qgadSvNyU'
+    },
+    'fifth_harmony': {
+        'worth_it': 'YBHQbu5rbdQ',
+        'work_from_home': '5GL9JoH4Sws',
+        'dont_say_you_love_me' : 'ju_inUnrLc4'
+    },
+    'ariana_grande': {
+        '7rings': 'QYh6mYIJG2Y',
+        'no_tears_left_to_cry': 'ffxKSjUwKdU',
+        'thanku_next' : 'gl1aHhXnN1k'
+    },
+    'doja_cat': {
+        'say_so': 'pok8H_KF1FA',
+        'boss_bitch': 'RsW66teC0BQ',
+        'juicy' : 'YIALlhlyqO4'
+    }
+}
